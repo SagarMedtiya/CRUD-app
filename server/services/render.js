703e44d2664aca42ba49ed79e70681806a019c3e
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-27 21:21:21 +0530

    configure axios

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,16 @@
+const axios = require('axios');
+const moment = require('moment');
 
 exports.homeRoutes = (req,res)=>{
-    res.render('index');
+    //Make a GET request to api/tasks
+    axios.get('http://localhost:3000/api/tasks')
+        .then(function(response){
+            res.render('index', { tasks: response.data, moment: moment});
+        })
+        .catch(err=>{
+            res.send(err)
+        })
+    
 }
 
 exports.add_task = (req,res)=>{
