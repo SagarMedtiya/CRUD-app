e5e93aee8186fde62258079b61ed6c29dfa6e7f3
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-27 08:01:55 +0530

    add api functionality

---
+++ server/controller/controller.js
@@ -0,0 +1,22 @@
+var TaskDB = require('../model/model');
+
+
+//create and save new task
+exports.create = (req,res)=>{
+    
+}
+
+//retrieve and retuen all tasks
+exports.find() = (req,res)=>{
+
+} 
+
+//update a new identified task by task id
+exports.update()=(req,res)=>{
+
+}
+
+//delete a task with specified task id
+exports.delete()= (req,res)=>{
+    
+}
