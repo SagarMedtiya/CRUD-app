a4e0200713d91556ecd527f27092163396d9b0b8
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-28 08:03:33 +0530

    configure ajax functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,40 @@
 $("#add_task").submit(function(event){
     alert("Data submitted successfully")
 })
+
+$('#update_task').submit(function(event){
+    event.preventDefault();
+    var unindexed_array = $(this).serializeArray();
+    var data={}
+    $.map(unindexed_array,function(n,i){
+        data[n['name']] = n['value']
+    })
+    console.log(unindexed_array)
+
+    var request = {
+        "url": `http://localhost:3000/api/tasks/${data.id}`,
+        "method": "PUT",
+        "data": data
+    }
+    $.ajax(request).done(function(response){
+        alert('Task updated successfully')
+    })
+})
+
+if(window.location.pathname == '/'){
+    $onDelete = $(".table tbody td a.delete");
+    $onDelete.click(function(){
+        var id = $(this).attr("data-id");
+        var request = {
+            "url": `http://localhost:3000/api/tasks/${id}`,
+            "method": "DELETE"
+        }
+        if(confirm("Do you really want to delete the task?")){
+            $.ajax(request).done(function(response){
+                alert('Task deleted successfully');
+                location.reload()
+            })
+        }
+    })
+    
+}
